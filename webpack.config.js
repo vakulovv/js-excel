070229612a213c3324fd07baaeff3db2e6958a22
Виталий Вакулов <vetal09@MacBook-Pro-Vitalij.local>
2021-01-16 22:07:34 +0300

    Finish project config

---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+// Webpack работает на Nodejs
+
+// Экспортируем объект
+
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const fileName = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+//
+
+const jsLoaders = () => {
+    const loaders = [
+        {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    ]
+
+    if(isDev) {
+        loaders.push('eslint-loader')
+    }
+
+    return loaders
+}
+console.log('IS PROD', isProd)
+console.log('IS DEV', isDev)
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'), // путь к исходникам
+    mode: "development",
+    entry: ['@babel/polyfill','./index.js'], // Главный файл, с которого все начинается
+    output: {
+        filename: fileName('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js'],
+        // import '../../../../core/Component
+        // import '@core/Component
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core')
+        }
+    },
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+        port: 3003,
+        hot: isDev,
+        open: true
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: 'index.html',
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd
+            }
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }],
+        }),
+        new MiniCssExtractPlugin({
+            filename: fileName('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+
+                        }
+                    },
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+
+                use: jsLoaders()
+
+            }
+        ],
+    }
+}

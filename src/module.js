7b02088e4fc67e13c77bc926d1221d821bf071fc
Виталий Вакулов <vetal09@MacBook-Pro-Vitalij.local>
2021-01-18 00:07:01 +0300

    Finish css layout

--- src/module.js
+++ src/module.js
@@ -1,7 +1,8 @@
 console.log('Work Module.js')
 
 async function start() {
-    return await Promise.resolve('async is working!')
+  return await Promise.resolve('async is working!!!')
 }
 
 start().then(console.log)
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@
 // Экспортируем объект
 
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
@@ -15,89 +16,88 @@ const fileName = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 //
 
 const jsLoaders = () => {
-    const loaders = [
-        {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }
-    ]
-
-    if(isDev) {
-        loaders.push('eslint-loader')
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
     }
+  ]
 
-    return loaders
+  if (isDev) {
+    //  loaders.push('eslint-loader')
+  }
+
+  return loaders
 }
 console.log('IS PROD', isProd)
 console.log('IS DEV', isDev)
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'), // путь к исходникам
-    mode: "development",
-    entry: ['@babel/polyfill','./index.js'], // Главный файл, с которого все начинается
-    output: {
-        filename: fileName('js'),
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.js'],
-        // import '../../../../core/Component
-        // import '@core/Component
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src/core')
-        }
-    },
-    devtool: isDev ? 'source-map' : false,
-    devServer: {
-        port: 3003,
-        hot: isDev,
-        open: true
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HTMLWebpackPlugin({
-            template: 'index.html',
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd
-            }
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist')
-                }],
-        }),
-        new MiniCssExtractPlugin({
-            filename: fileName('css')
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-
-                        }
-                    },
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-
-                use: jsLoaders()
-
-            }
-        ],
+  context: path.resolve(__dirname, 'src'), // путь к исходникам
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'], // Главный файл, с которого все начинается
+  output: {
+    filename: fileName('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js'],
+    // import '../../../../core/Component
+    // import '@core/Component
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core')
     }
+  },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: 3003,
+    hot: isDev,
+    open: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      }
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }],
+    }),
+    new MiniCssExtractPlugin({
+      filename: fileName('css')
+    }),
+    new ESLintPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {}
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+
+        use: jsLoaders()
+
+      }
+    ],
+  }
 }

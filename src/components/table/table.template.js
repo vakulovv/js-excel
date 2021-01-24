6e524fb2d9b67763f82b5666e39d3e9754b74e28
Виталий Вакулов <vetal09@MacBook-Pro-Vitalij.local>
2021-01-24 21:39:30 +0300

    Add comment

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -28,7 +28,7 @@ function toChar(_, index) {
 }
 
 export function createTable(rowsCount = 15) {
-  const colsCount = CODES.Z - CODES.A + 1
+  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
   const rows = []
 
   const cols = new Array(colsCount)
